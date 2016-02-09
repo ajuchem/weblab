91a5d5bac80a9d576bb9f812883f75fcd68526ad
Allie Daudt <ajuchem@daudt.com>
2016-02-08 19:04:26 -0700

    Simple program to find the sum of an array

---
+++ intro/array.js
@@ -0,0 +1,10 @@
+// Simple program to get the sum of an array
+var samples = [34, 18, 26, 44, 95];
+var total = 0;
+
+for(var i=0; i < samples.length; i++) {
+  total += samples[i];
+  // console.log(i, samples[i], total);
+}
+
+console.log(total);
