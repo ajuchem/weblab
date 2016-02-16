41240cce213f5bb54156e120a5c6c3dc2a94c313
ajuchem <ajuchem@daudt.com>
2016-02-15 20:08:52 -0700

    Transform and transitions

--- htmlCSS/week2/inclass1.js
+++ htmlCSS/week2/inclass1.js
@@ -1,9 +1,15 @@
-// ADDING CLASSES DYNAMICALLY WITH JQUERY
+ /* ADDING CLASSES DYNAMICALLY WITH JQUERY
+    TO WORK WITH TRANSITIONS AND TRANSFORMS */
 
 $(function () {
     var block = $('.block');
     $('.button1').on('click', function() {
         block.toggleClass('rounded');
+
+    })
+
+    $('.button2').on('click', function() {
+        block.toggleClass('moved');
 
     })
 })
