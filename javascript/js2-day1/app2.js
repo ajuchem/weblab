41573318a8637bdbf2d7a65709f75e954e12dbab
ajuchem <ajuchem@daudt.com>
2016-03-07 19:28:46 -0700

    Install ejs

---
+++ javascript/js2-day1/app2.js
@@ -0,0 +1,19 @@
+console.log('hi');
+
+var properties = [
+  {
+    ownerName: "Allie Daudt",
+    address: "123 Main St"
+  },
+  {
+    ownerName: "Bob Johnson",
+    address: "12 Three St"
+  },
+  {
+    ownerName: "Mary Smith",
+    address: "500 1st Ave"
+  }
+];
+
+console.log(properties[0].ownerName);
+console.log(properties);
